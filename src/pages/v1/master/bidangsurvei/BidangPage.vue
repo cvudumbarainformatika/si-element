bedf4367182443d10504c7c55d8fb9e28af221c1
farhandardiri <faran.f4124n@gmail.com>
2022-12-12 14:53:07 +0700

    pisah bidang survei

---
+++ src/pages/v1/master/bidangsurvei/BidangPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="row q-col-gutter-lg">
+    <div class="col-md-12 col-xs-12">
+      <app-card :is-header="false">
+        <template #content>
+          <app-table
+            title="Data bidang survei"
+            :columns="table.columns"
+            :column-hide="table.columnHide"
+            :items="table.items"
+            :meta="table.meta"
+            :per-page="table.params.per_page"
+            :order-by="table.params.order_by"
+            :sort="table.params.sort"
+            :loading="table.loading"
+            :to-search="table.params.q"
+            btn-delete
+            @set-order="table.setOrder"
+            @set-row="table.setPerPage"
+            @goto="table.setPage"
+            @delete-ids="table.deletesData"
+            @delete="table.deletesData"
+            @find="table.setSearch"
+            @refresh="table.refreshTable"
+            @new-data="store.newData"
+            @edit-data="store.editData"
+          />
+        </template>
+      </app-card>
+    </div>
+  </div>
+  <form-dialog v-model="store.isOpen" />
+</template>
+<script setup>
+
+import { useBidangSurveiTable } from 'src/stores/master/bidangsurvei/table'
+import { useBidangSurveiFormStore } from 'src/stores/master/bidangsurvei/form'
+import formDialog from './FormDialog.vue'
+const table = useBidangSurveiTable()
+const store = useBidangSurveiFormStore()
+
+table.getDataTable()
+</script>
